b8ad520dddc2484c943e5165766c35817b4fc032
Chris Roat <1053153+chrisroat@users.noreply.github.com>
2023-09-19 20:11:24 -0700

    Add webserver

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, send_from_directory
+
+FIGURES = {
+    "votes": "Results",
+    "corr": "Vote correlations",
+    # "frac": "Vote overlap",
+    # "num_votes": "#Votes/Ballot",
+    # "num_winners": "#Winners/Ballot",
+    "num_votes_winners": "#Votes X #Winners/Ballot",
+    "combos": "Ballot combinations",
+}
+ELECTIONS = {
+    "2020 SF Community College District": "2020/ca/city/san_francisco/general/community_college_district/san_francisco/board",
+    "2020 SF School Board": "2020/ca/city/san_francisco/general/school_district/san_francisco/board",
+    "2020 Los Altos City Council": "2020/ca/county/santa_clara/general/city/los_altos/council",
+}
+
+app = Flask(__name__)
+
+
+@app.route("/data/2020/<path:path>")
+def data(path):
+    return send_from_directory("2020", path)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", titles=ELECTIONS)
+
+
+@app.route("/election/<title>")
+def election(title):
+    path = ELECTIONS[title]
+    return render_template("election.html", title=title, path=path, figures=FIGURES)
+
+
+if __name__ == "__main__":
+    app.run()
